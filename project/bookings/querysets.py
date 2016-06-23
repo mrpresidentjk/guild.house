7c6afb8b0759b0dd72e770fdddb0aead3097bf48
Elena Williams <git@elena.net.au>
2016-06-23 19:54:54 +1000

    Add filter for big bookings.

--- project/bookings/querysets.py
+++ project/bookings/querysets.py
@@ -3,12 +3,16 @@ from __future__ import absolute_import, unicode_literals
 from django.contrib.sites.models import Site
 from django.db import models
 from django.utils import timezone
+from .settings import BIG_BOOKING
 
 
 class QuerySet(models.query.QuerySet):
 
     def active(self):
         return self.current_site().exclude(status="Cancelled")
+
+    def big(self):
+        return self.active().filter(party_size__gte=BIG_BOOKING)
 
     def current_site(self):
         site = Site.objects.get_current()
--- project/bookings/settings.py
+++ project/bookings/settings.py
@@ -7,4 +7,7 @@ BOOKINGS_PAGINATE_BY = getattr(settings, 'BOOKINGS_BOOKING_PAGINATE_BY', 10)
 
 BOOKINGS_FEED = getattr(settings, 'BOOKINGS_BOOKING_FEED', 10)
 
+# If a booking is greater or equal to this number it is treated as a larger sized booking.
+BIG_BOOKING = getattr(settings, 'BOOKINGS_BIG_BOOKING', 10)
+
 TIME_ZONE = settings.TIME_ZONE
