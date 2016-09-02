12f2dec7953c9087a1ff5a85806375eea9eada50
Elena Williams <git@elena.net.au>
2016-09-03 00:35:13 +1000

    Add flatpages to sitemaps.
    While we're there.

--- project/sitemaps.py
+++ project/sitemaps.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from django.contrib.flatpages.sitemaps import FlatPageSitemap as flatpages
 from project.blog.sitemaps import sitemaps as blog
 from project.library.sitemaps import sitemaps as library
 from project.site.sitemaps import sitemaps as site
 
 
 sitemaps = {}
+sitemaps['flatpages'] = flatpages
 sitemaps.update(blog)
 sitemaps.update(site)
