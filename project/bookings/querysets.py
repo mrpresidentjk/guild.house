9649ba30b75132017cfc395d3e5fe99aa1b9c846
Elena Williams <git@elena.net.au>
2016-09-09 21:57:59 +1000

    Add queryset for `pax` count.

--- project/bookings/querysets.py
+++ project/bookings/querysets.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, unicode_literals
 from django.contrib.sites.models import Site
 from django.db import models
+from django.db.models import Sum
 from django.utils import timezone
 from .settings import BIG_BOOKING
 
@@ -13,6 +14,9 @@ class QuerySet(models.query.QuerySet):
 
     def big(self):
         return self.active().filter(party_size__gte=BIG_BOOKING)
+
+    def pax(self):
+        return self.active().aggregate(Sum('party_size'))
 
     def current_site(self):
         site = Site.objects.get_current()
