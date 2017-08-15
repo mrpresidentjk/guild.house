bb7f2be1d9997f6a318918939d2a497512ee7995
Elena Williams <git@elena.net.au>
2017-08-16 04:08:49 +1000

    Add admin and __str__ to rolodex

--- project/rolodex/models.py
+++ project/rolodex/models.py
@@ -5,7 +5,13 @@ class Email(models.Model):
 
     email = models.EmailField()
 
+    def __str__(self):
+        return self.email
+
 
 class Phone(models.Model):
 
     phone = models.CharField(max_length=16)
+
+    def __str__(self):
+        return self.phone
