0c3915049a416d8eafb284ff8b30cbd47b2072c1
Elena Williams <git@elena.net.au>
2017-08-16 03:28:18 +1000

    Move AUPhoneNumberField to project fields
    For reuse in members app.

--- project/bookings/forms.py
+++ project/bookings/forms.py
@@ -1,42 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-import re
-from .models import Booking
-from .settings import DURATION_SELECTION, BOOKING_TIMES_CHOICES
-from datetime import date, timedelta, datetime
 from django import forms
-from django.core.validators import EMPTY_VALUES
-from django.forms import ValidationError
-from django.forms.fields import CharField
-from django.utils.encoding import force_text
 from tinymce.widgets import TinyMCE
 
-
-PHONE_DIGITS_RE = re.compile(r'^(\d{8,10})$')
-
-
-class AUPhoneNumberField(CharField):
-    """
-    A form field that validates input as an Australian phone number.
-    Valid numbers have ten digits.
-    """
-    default_error_messages = {
-        'invalid': 'Enter a valid phone number.'
-    }
-
-    def clean(self, value):
-        """
-        Validate a phone number. Strips parentheses, whitespace and hyphens.
-        """
-        super(AUPhoneNumberField, self).clean(value)
-        if value in EMPTY_VALUES:
-            return ''
-        value = re.sub('(\(|\)|\s+|-)', '', force_text(value))
-        phone_match = PHONE_DIGITS_RE.search(value)
-
-        if phone_match:
-            return '%s' % phone_match.group(1)
-        raise ValidationError(self.error_messages['invalid'])
+from project.fields import AUPhoneNumberField
+from .models import Booking
+from .settings import DURATION_SELECTION, BOOKING_TIMES_CHOICES
 
 
 class BookingAdminForm(forms.ModelForm):
---
+++ project/fields.py
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-
+import re
+from django.core.validators import EMPTY_VALUES
+from django.forms import ValidationError
+from django.forms.fields import CharField
+from django.utils.encoding import force_text
+
+
+PHONE_DIGITS_RE = re.compile(r'^(\d{8,10})$')
+
+
+class AUPhoneNumberField(CharField):
+    """
+    A form field that validates input as an Australian phone number.
+    Valid numbers have ten digits.
+    """
+    default_error_messages = {
+        'invalid': 'Enter a valid phone number.'
+    }
+
+    def clean(self, value):
+        """
+        Validate a phone number. Strips parentheses, whitespace and hyphens.
+        """
+        super(AUPhoneNumberField, self).clean(value)
+        if value in EMPTY_VALUES:
+            return ''
+        value = re.sub('(\(|\)|\s+|-)', '', force_text(value))
+        phone_match = PHONE_DIGITS_RE.search(value)
+
+        if phone_match:
+            phone = phone_match.group(1)
+            if len(phone) == 8 or (len(phone) == 10 and str(phone[0]) == "0"):
+                return '%s'.format(phone)
+        raise ValidationError(self.error_messages['invalid'])
