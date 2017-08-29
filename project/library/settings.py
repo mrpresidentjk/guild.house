064bfd45fe884bd15210dc5063bf49504358ff20
Elena Williams <git@elena.net.au>
2017-08-30 02:27:41 +1000

    Add library settings

---
+++ project/library/settings.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+CATEGORY_CHOICES = [
+    ('Abstract', 'Abstract'),
+    ('Classic', 'Classic'),
+    ('Dexterity', 'Dexterity'),
+    ('Kids', 'Kids'),
+    ('Party ', 'Party '),
+    ('Light Strategy', 'Light Strategy'),
+    ('Strategy', 'Strategy'),
+    ('Trivia', 'Trivia'),
+    ('Word', 'Word'),
+]
