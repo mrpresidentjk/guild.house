0626d521dcabbdbc2c27fdad37e740dbe860132b
Elena Williams <git@elena.net.au>
2017-09-10 13:01:04 +1000

    Refactor import script to allow for Membership import #19

--- project/members/scripts.py
+++ project/members/scripts.py
@@ -7,22 +7,43 @@ Script to generate members from spreadsheet one off.
 - Generate `rolodex.Email` and `rolodex.Phone` objects.
 
 """
-from .models import Member
+from .models import Member, Membership
 from project.utils import convert_tsv
 
 
 # Synchronising scrape from Revel to bookings system.
 # To be automated daily.
 
+def create_member(kwargs):
 
-def import_members(scrape):
+    obj = Member(number=kwargs.pop('number'), **kwargs)
+    obj.save()
+    return obj
 
-    data = convert_tsv(scrape)
 
+def create_membership(kwargs):
+
+    member = Member.objects.get_or_create(number=kwargs.pop('number'))
+    obj = Membership(member=member, **kwargs)
+    obj.save()
+    return obj
+
+
+def import_members(text):
+
+    data = convert_tsv(text)
     success = []
     for kwargs in data:
-        obj = Member(number=kwargs.pop('number'), **kwargs)
-        obj.save()
-        print(obj)
+        obj = create_member(kwargs=kwargs)
+        success.append(obj)
+    return success
+
+
+def import_memberships(text):
+
+    data = convert_tsv(text)
+    success = []
+    for kwargs in data:
+        obj = create_membership(kwargs=kwargs)
         success.append(obj)
     return success
