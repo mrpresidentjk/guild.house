839e13a9658c10bca6cfd7a49734571f9aee9b04
Elena Williams <git@elena.net.au>
2017-09-21 11:27:44 +1000

    Add created_at/checked_at datetime to rolodex Email/Phone and migration

--- project/rolodex/models.py
+++ project/rolodex/models.py
@@ -1,9 +1,15 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 
 class AbstractRolodex(models.Model):
 
     is_working = models.BooleanField(default=True)
+
+    created_at = models.DateField(auto_now_add=True)
+
+    last_checked_at = models.DateTimeField(auto_now=True,
+                                           editable=True)
 
     class Meta:
         abstract = True
