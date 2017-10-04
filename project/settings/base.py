1eaa80877ad4780f843c231bd380c4d001af329c
Elena Williams <git@elena.net.au>
2017-10-05 02:21:08 +1100

    Merge live master

--- project/settings/base.py
+++ project/settings/base.py
@@ -162,49 +162,49 @@ LOGIN_URL = '/admin/'
 # Logging
 # https://docs.getsentry.com/hosted/clients/python/integrations/django/#integration-with-logging
 
-## LOGGING = {
-##     'version': 1,
-##     'disable_existing_loggers': True,
-##     'root': {
-##         'level': 'WARNING',
-##         'handlers': ['sentry'],
-##     },
-##     'formatters': {
-##         'verbose': {
-##             'format': '%(levelname)s %(asctime)s %(module)s %(process)d '
+# LOGGING = {
+# 'version': 1,
+# 'disable_existing_loggers': True,
+# 'root': {
+# 'level': 'WARNING',
+# 'handlers': ['sentry'],
+# },
+# 'formatters': {
+# 'verbose': {
+# 'format': '%(levelname)s %(asctime)s %(module)s %(process)d '
 ##                       '%(thread)d %(message)s'
-##         },
-##     },
-##     'handlers': {
-##         'sentry': {
-##             'level': 'ERROR',
-##             'class':
-##                 'raven.contrib.django.raven_compat.handlers.SentryHandler',
-##         },
-##         'console': {
-##             'level': 'DEBUG',
-##             'class': 'logging.StreamHandler',
-##             'formatter': 'verbose'
-##         }
-##     },
-##     'loggers': {
-##         'django.db.backends': {
-##             'level': 'ERROR',
-##             'handlers': ['console'],
-##             'propagate': False,
-##         },
-##         'raven': {
-##             'level': 'DEBUG',
-##             'handlers': ['console'],
-##             'propagate': False,
-##         },
-##         'sentry.errors': {
-##             'level': 'DEBUG',
-##             'handlers': ['console'],
-##             'propagate': False,
-##         },
-##     },
-## }
+# },
+# },
+# 'handlers': {
+# 'sentry': {
+# 'level': 'ERROR',
+# 'class':
+# 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+# },
+# 'console': {
+# 'level': 'DEBUG',
+# 'class': 'logging.StreamHandler',
+# 'formatter': 'verbose'
+# }
+# },
+# 'loggers': {
+# 'django.db.backends': {
+# 'level': 'ERROR',
+# 'handlers': ['console'],
+# 'propagate': False,
+# },
+# 'raven': {
+# 'level': 'DEBUG',
+# 'handlers': ['console'],
+# 'propagate': False,
+# },
+# 'sentry.errors': {
+# 'level': 'DEBUG',
+# 'handlers': ['console'],
+# 'propagate': False,
+# },
+# },
+# }
 
 
 # AWS
