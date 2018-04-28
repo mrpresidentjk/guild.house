c7c6662e2f415d77d359eab9a58dd83e63819f84
Elena Williams <git@elena.net.au>
2018-04-28 21:17:08 +1000

    Add library urls to project urls

--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,9 @@ urlpatterns = [
     # Menu
     url(r'^menus/', include('project.menus.urls')),
 
+    # Games Library
+    url(r'^games/', include('project.library.urls')),
+
     # Bookings
     url(r'^bookings/', include('project.bookings.urls')),
 
